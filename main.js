429bd7b9c3f9199c0e266e5ddc8cc7ebaa392689
Brenda <brenda.nozella@gmail.com>
2022-04-09 13:20:59 -0300

    keys-fix

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+            elemento.play();
+        }
+    else {
+        //alert('Elemento não encontrado');
+        console.log ('Elemento não encontrado ou seletor inválido')
+    } 
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    //template string
-    const idAudio = `#som_${instrumento}`;
-
-    //console.log(instrumento);
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
-    contador = contador + 1;
+    tecla.onkeydown = function (evento) {
 
-    //console.log(contador);
+        if (evento.code === 'Space' || evento.code === 'Enter')
+            tecla.classList.add('ativa');
+        }
+
+        tecla.onkeyup = function () {
+            tecla.classList.remove('ativa');
+    }
+
 }
